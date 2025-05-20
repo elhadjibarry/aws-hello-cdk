6084366a579a584fc66fedc2b749bf3005c996fb
Elhadji Barry <elhadji.barry@gmail.com>
2025-05-19 20:12:59 -0400

    Add Hello World Lambda function

--- src/main/java/com/myorg/HelloCdkApp.java
+++ src/main/java/com/myorg/HelloCdkApp.java
@@ -26,12 +26,10 @@ public class HelloCdkApp {
 
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
-                /*
                 .env(Environment.builder()
-                        .account("123456789012")
+                        .account("484907526888")
                         .region("us-east-1")
                         .build())
-                */
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
                 .build());
--- src/main/java/com/myorg/HelloCdkStack.java
+++ src/main/java/com/myorg/HelloCdkStack.java
@@ -1,10 +1,17 @@
 package com.myorg;
 
 import software.constructs.Construct;
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.lambda.Code;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.FunctionUrl;
+import software.amazon.awscdk.services.lambda.FunctionUrlAuthType;
+import software.amazon.awscdk.services.lambda.FunctionUrlOptions;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class HelloCdkStack extends Stack {
     public HelloCdkStack(final Construct scope, final String id) {
@@ -15,10 +22,30 @@ public class HelloCdkStack extends Stack {
         super(scope, id, props);
 
         // The code that defines your stack goes here
+        Function helloFunction = Function.Builder.create(this, "HelloFunction")
+                .runtime(Runtime.NODEJS_20_X)
+                .handler("index.handler")
+                .code(Code.fromInline(
+                        "exports.handler = async function(event) {" +
+                                " return {" +
+                                " statusCode: 200," +
+                                " body: JSON.stringify('Hello CDK!')" +
+                                " };" +
+                                "};"))
+                .build();
+
+        FunctionUrl helloFunctionUrl = helloFunction.addFunctionUrl(FunctionUrlOptions.builder()
+                .authType(FunctionUrlAuthType.NONE)
+                .build());
+
+        CfnOutput.Builder.create(this, "HelloFunctionUrlOutput")
+                .value(helloFunctionUrl.getUrl())
+                .description("The URL of the Hello Function")
+                .build();
 
         // example resource
         // final Queue queue = Queue.Builder.create(this, "HelloCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        // .visibilityTimeout(Duration.seconds(300))
+        // .build();
     }
 }
